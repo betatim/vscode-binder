97e3ed6ba6984a35c9d1a47b471ace235d36cfe9
Tim Head <betatim@gmail.com>
2019-03-13 09:40:36 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="jupyter-vscode-proxy",
+    version='0.1',
+    url="https://github.com/betatim/vscode-binder",
+    author="Tim Head",
+    description="Jupyter extension to proxy VS Code",
+    packages=setuptools.find_packages(),
+	  keywords=['Jupyter'],
+  	classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'jupyter-server-proxy'
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'vscode = jupyter_vscode_proxy:setup_vscode',
+        ]
+    },
+    package_data={
+        'jupyter_vscode_proxy': ['icons/*'],
+    },
+)
