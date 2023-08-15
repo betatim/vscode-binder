796a39b000617898b2fb2e2bc559ac89cbd8fccc
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-08-15 16:40:30 +0200

    Add openvscode-server as executable option.

--- jupyter_vscode_proxy/__init__.py
+++ jupyter_vscode_proxy/__init__.py
@@ -27,8 +27,31 @@ def _get_vscode_cmd(port):
     return cmd
 
 
+def _get_openvscode_cmd(port):
+    executable = "openvscode-server"
+    if not shutil.which(executable):
+        raise FileNotFoundError("Can not find openvscode-server in PATH")
+
+    cmd = [
+        executable,
+        "--without-connection-token",
+        "--telemetry-level off",
+        "--port=" + str(port),
+    ]
+
+    if (extensions_dir := os.getenv("CODE_EXTENSIONSDIR", None)):
+        cmd += ["--extensions-dir", extensions_dir]
+
+    # Start openvscode in CODE_WORKINGDIR env variable if set
+    # If not, start in 'current directory'.
+    working_dir = os.getenv("CODE_WORKINGDIR", ".")
+    cmd.append(working_dir)
+    return cmd
+
+
 _CODE_EXECUTABLE_CMD_MAP = {
     "code-server": _get_vscode_cmd,
+    "openvscode-server": _get_openvscode_cmd,
 }
 
 
