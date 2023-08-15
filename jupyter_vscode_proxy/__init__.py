1de6cee2a59288fa7bd2d8dddc35f2ea0b69296d
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-08-15 16:34:26 +0200

    Move _get_vscode_cmd outside of setup_vscode function.

--- jupyter_vscode_proxy/__init__.py
+++ jupyter_vscode_proxy/__init__.py
@@ -1,33 +1,33 @@
 import os
 import shutil
 
+def _get_vscode_cmd(port):
+    executable = "code-server"
+    if not shutil.which(executable):
+        raise FileNotFoundError("Can not find code-server in PATH")
+    
+    # Start vscode in CODE_WORKINGDIR env variable if set
+    # If not, start in 'current directory', which is $REPO_DIR in mybinder
+    # but /home/jovyan (or equivalent) in JupyterHubs
+    working_dir = os.getenv("CODE_WORKINGDIR", ".")
+
+    extensions_dir = os.getenv("CODE_EXTENSIONSDIR", None)
+    cmd = [
+        executable,
+        "--auth",
+        "none",
+        "--disable-telemetry",
+        "--port=" + str(port),
+    ]
+
+    if extensions_dir:
+        cmd += ["--extensions-dir", extensions_dir]
+
+    cmd.append(working_dir)
+    return cmd
+
 
 def setup_vscode():
-    def _get_vscode_cmd(port):
-        executable = "code-server"
-        if not shutil.which(executable):
-            raise FileNotFoundError("Can not find code-server in PATH")
-        
-        # Start vscode in CODE_WORKINGDIR env variable if set
-        # If not, start in 'current directory', which is $REPO_DIR in mybinder
-        # but /home/jovyan (or equivalent) in JupyterHubs
-        working_dir = os.getenv("CODE_WORKINGDIR", ".")
-
-        extensions_dir = os.getenv("CODE_EXTENSIONSDIR", None)
-        cmd = [
-            executable,
-            "--auth",
-            "none",
-            "--disable-telemetry",
-            "--port=" + str(port),
-        ]
-
-        if extensions_dir:
-            cmd += ["--extensions-dir", extensions_dir]
-
-        cmd.append(working_dir)
-        return cmd
-
     return {
         "command": _get_vscode_cmd,
         "timeout": 300,
